59b23553311fd55d46cd001899d9f62dda80a6d7
naisepaul <>
2024-02-11 16:52:48 +0000

    Add Settings.py file

--- petlovers/settings.py
+++ petlovers/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1!t_o_v^-v^j6ayo!h0+*kc&0h7j!gf&%wu5$2otsknu@t1$=1'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
